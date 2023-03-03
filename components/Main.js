fcf7862b408f7f9618d2eedba603c209aff44935
unknown <kamalapurnaveen147@gmail.com>
2023-03-04 00:53:45 +0530

    very simple project is added to git

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Interesting Facts of React</h1>
+            <ul className="main--facts">
+                <li>React is Composable </li>
+                <li>React is declarative</li>
+                <li>Huge Community</li>
+                <li>Most Famous Frontend Tool</li>
+                <li>Various Sources to learn</li>
+            </ul>
+        </main>
+    )
+}
